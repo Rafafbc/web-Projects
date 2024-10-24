4a11fdad73bfec13a572ea672a7a7544e90ab09a
Rafael Baldessini Campanini <rafaelcampanini90@gmail.com>
2024-10-24 02:05:43 -0300

    1° commit do repositório atualizado!

---
+++ LINCE_ESC-Project/orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        productName: 'Java Script Tutorial',
+        productNumber: '78543',
+        productStatus: 'Due',
+        status: 'Pending'
+    },
+    {
+        productName: 'CSS Full Course',
+        productNumber: '89354',
+        productStatus: 'Refunded',
+        status: 'Declined'
+    },
+    {
+        productName: 'Flex-Box Tutorial',
+        productNumber: '36452',
+        productStatus: 'Paid',
+        status: 'Active'
+    },
+]
--- readme.md
+++ readme.md
@@ -1,1 +1,4 @@
-## Testing my direct commiting by Vscode
+# WEB-Projects
+## Projetos Web
+- Listagem-Pokemon: <rafafbc.github.io/web-projects/Listagem-Pokemon/>
+- LINCE_ESC-Project <rafafbc.github.io/web-projects/LINCE_ESC-Project/>
