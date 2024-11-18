240c3e841a06b195bef447f1cae5936605493475
Rafael Baldessini Campanini <rafaelcampanini90@gmail.com>
2024-11-18 02:43:52 -0300

    plot dos gráficos das 4 temperaturas feito

--- LINCE_ESC-Project/plotChart.js
+++ LINCE_ESC-Project/plotChart.js
@@ -107,9 +107,200 @@ function plotPressChart(quantity = 100) {
         }
     });
 
+    console.log("Gráfico de pressão plotado com sucesso!");
+}
+// Função para plotar o gráfico de Temperatura
+function plotTempChart(quantity = 100) {
+    // Seleciona a div desejada
+    const infoData1 = document.getElementById("data-info_1");
+
+    // Remove o canvas existente se já estiver presente
+    const existingCanvas = document.getElementById('my-Chart');
+    if (existingCanvas) {
+        existingCanvas.remove();
+    }
+
+    // Cria um novo elemento <canvas> para exibir o gráfico
+    const plotedChart = document.createElement("canvas");
+    plotedChart.id = 'my-Chart';
+    plotedChart.width = 800;  // Largura
+    plotedChart.height = 400; // Altura
+    infoData1.appendChild(plotedChart);
+
+    console.log("plotChart!!!!!");
+
+    // Recupera os dados completos
+    const data = getDataArrays();
+    console.log("Dados totais: ", data);
+
+    // Filtra os dados com base na quantidade selecionada
+    const filteredData = filterDataByQuantity(data, quantity);
+    console.log("Dados filtrados: ", filteredData);
+
+    // Define o contexto 2D do novo canvas
+    const ctx = plotedChart.getContext('2d');
+
+    // Verifica se já existe um gráfico. Se sim, destrói o gráfico anterior.
+    if (myChart !== null) {
+        myChart.destroy();
+    }
+
+    // Cria o gráfico
+    myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: filteredData.timestamps, // Eixo X
+            datasets: [
+                {
+                    label: 'Temperatura',
+                    data: filteredData.temperaturas, // Eixo Y
+                    borderColor: 'rgb(210, 72, 72, 1)',
+                    backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                    borderWidth: 2
+                }
+            ]
+        },
+        options: {
+            responsive: true,
+            //maintainAspectRatio: false, // Permite personalizar o tamanho
+            plugins: {
+                title: {
+                    display: true, // Habilita o título
+                    text: 'Série temporal dos dados', // Texto do título
+                    font: {
+                        size: 18, // Tamanho da fonte do título
+                        weight: 'bold' // Peso da fonte
+                    },
+                    //color: '#333', // Cor do título
+                    padding: {
+                        top: 10, // Espaçamento superior
+                        bottom: 10 // Espaçamento inferior
+                    }
+                }
+            },
+            scales: {
+                x: {
+                    title: {                // Título do eixo x
+                        display: true,
+                        text: 'Tempo',
+                        //color: '#333'
+                    },
+                    ticks: {                // Rótulos do eixo x
+                        //color: '#333'
+                    }
+                },
+                y: {
+                    title: {                // Título do eixo y
+                        display: true,
+                        text: 'Valores',
+                        //color: '#333'
+                    },
+                    ticks: {                // Rótulos do eixo y
+                        //color: '#333'
+                    }
+                }
+            }
+        }
+    });
+
+    console.log("Gráfico de temperatura plotado com sucesso!");
+}
+// Função para plotar o gráfico de Umidade
+function plotHumidChart(quantity = 100) {
+    // Seleciona a div desejada
+    const infoData1 = document.getElementById("data-info_1");
+
+    // Remove o canvas existente se já estiver presente
+    const existingCanvas = document.getElementById('my-Chart');
+    if (existingCanvas) {
+        existingCanvas.remove();
+    }
+
+    // Cria um novo elemento <canvas> para exibir o gráfico
+    const plotedChart = document.createElement("canvas");
+    plotedChart.id = 'my-Chart';
+    plotedChart.width = 800;  // Largura
+    plotedChart.height = 400; // Altura
+    infoData1.appendChild(plotedChart);
+
+    console.log("plotChart!!!!!");
+
+    // Recupera os dados completos
+    const data = getDataArrays();
+    console.log("Dados totais: ", data);
+
+    // Filtra os dados com base na quantidade selecionada
+    const filteredData = filterDataByQuantity(data, quantity);
+    console.log("Dados filtrados: ", filteredData);
+
+    // Define o contexto 2D do novo canvas
+    const ctx = plotedChart.getContext('2d');
+
+    // Verifica se já existe um gráfico. Se sim, destrói o gráfico anterior.
+    if (myChart !== null) {
+        myChart.destroy();
+    }
+
+    // Cria o gráfico
+    myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: filteredData.timestamps, // Eixo X
+            datasets: [
+                {
+                    label: 'Umidade',
+                    data: filteredData.umidades, // Eixo Y
+                    borderColor: 'rgb(34, 116, 156, 1)',
+                    backgroundColor: 'rgba(54, 162, 235, 0.2)',
+                    borderWidth: 2
+                }
+            ]
+        },
+        options: {
+            responsive: true,
+            //maintainAspectRatio: false, // Permite personalizar o tamanho
+            plugins: {
+                title: {
+                    display: true, // Habilita o título
+                    text: 'Série temporal dos dados', // Texto do título
+                    font: {
+                        size: 18, // Tamanho da fonte do título
+                        weight: 'bold' // Peso da fonte
+                    },
+                    //color: '#333', // Cor do título
+                    padding: {
+                        top: 10, // Espaçamento superior
+                        bottom: 10 // Espaçamento inferior
+                    }
+                }
+            },
+            scales: {
+                x: {
+                    title: {                // Título do eixo x
+                        display: true,
+                        text: 'Tempo',
+                        //color: '#333'
+                    },
+                    ticks: {                // Rótulos do eixo x
+                        //color: '#333'
+                    }
+                },
+                y: {
+                    title: {                // Título do eixo y
+                        display: true,
+                        text: 'Valores',
+                        //color: '#333'
+                    },
+                    ticks: {                // Rótulos do eixo y
+                        //color: '#333'
+                    }
+                }
+            }
+        }
+    });
+
     console.log("Gráfico plotado com sucesso!");
 }
-
 // Função para plotar o gráfico de Lux
 function plotLuxChart(quantity = 100) {
     // Seleciona a div desejada
@@ -213,9 +404,26 @@ function handleDataQuantitySelection() {
 
     const quantity = parseInt(quantityInput.value);
 
-    // Chama a função de plotagem passando a quantidade de dados selecionada
-    plotPressChart(quantity);
-    plotLuxChart(quantity);
+    // Função para lidar com o clique do botão de Pressão e chamar o gráfico
+    document.getElementById('press-btn').addEventListener('click', function () {
+        console.log("Chamando plotPressChart com quantidade:", quantity);
+        plotPressChart(quantity); // Usa o valor atualizado de `quantity`
+    });
+    // Função para lidar com o clique do botão de Temperatura e chamar o gráfico
+    document.getElementById('temp-btn').addEventListener('click', function () {
+        console.log("Chamando plotTempChart com quantidade:", quantity);
+        plotTempChart(quantity); // Usa o valor atualizado de `quantity`
+    });
+    // Função para lidar com o clique do botão de Umidade e chamar o gráfico
+    document.getElementById('humid-btn').addEventListener('click', function () {
+        console.log("Chamando plotHumidChart com quantidade:", quantity);
+        plotHumidChart(quantity); // Usa o valor atualizado de `quantity`
+    });
+    // Função para lidar com o clique do botão de Umidade e chamar o gráfico
+    document.getElementById('lux-btn').addEventListener('click', function () {
+        console.log("Chamando plotLuxChart com quantidade:", quantity);
+        plotLuxChart(quantity); // Usa o valor atualizado de `quantity`
+    });
 }
 
 // Exemplo de como integrar com a interface de usuário (HTML)
@@ -231,5 +439,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
 // Expondo a função "plotChart" ao escopo global
 window.plotPressChart = plotPressChart;
+window.plotTempChart = plotTempChart;
+window.plotHumidChart = plotHumidChart;
 window.plotLuxChart = plotLuxChart;
 window.handleDataQuantitySelection = handleDataQuantitySelection;
--- LINCE_ESC-Project/references.md
+++ LINCE_ESC-Project/references.md
@@ -15,3 +15,17 @@
   - **Firebase Realtime Database:** <https://firebase.google.com/docs/database?hl=pt-br>
     - **Instalação e Configuração no JavaScript:** <https://firebase.google.com/docs/database/web/start?hl=pt-br>
   - **Firebase Authentication:** <https://firebase.google.com/docs/auth?hl=pt-br>
+  - **Adicionar o Firebase ao projeto JavaScript :** <https://firebase.google.com/docs/web/setup?hl=pt-br>
+  - **Atualizações em tempo real com Firebase Database:** <https://medium.com/aurum-tech/atualizações-em-tempo-real-com-firebase-database-84e67dffc486>
+  - **Configurar um app cliente em JavaScript do Firebase Cloud Messaging:** <https://firebase.google.com/docs/cloud-messaging/js/client?hl=pt-br#configure_web_credentials_with>
+  - **Adicione o SDK Admin do Firebase ao seu servidor :** <https://firebase.google.com/docs/admin/setup?hl=pt>
+
+# REST
+- **REST: Conceito e fundamentos:** <https://www.alura.com.br/artigos/rest-conceito-e-fundamentos?utm_term=&utm_campaign=%5BSearch%5D+%5BPerformance%5D+-+Dynamic+Search+Ads+-+Artigos+e+Conteúdos&utm_source=adwords&utm_medium=ppc&hsa_acc=7964138385&hsa_cam=11384329873&hsa_grp=164240702375&hsa_ad=703935132410&hsa_src=g&hsa_tgt=dsa-2276348409543&hsa_kw=&hsa_mt=&hsa_net=adwords&hsa_ver=3&gad_source=1&gclid=CjwKCAiAxea5BhBeEiwAh4t5K9mVxDKGLSFC-k72Qaae32HkuOohgJvpE1ts6VSmtUxUfSVcFiKG3xoCrUsQAvD_BwE>
+- **Interagindo de forma assíncrona com APIs REST usando os nós RESTAsyncRequest e RESTAsincResposta:** <https://www.ibm.com/docs/pt-br/integration-bus/10.0?topic=cera-interacting-asynchronously-rest-apis-by-using-restasyncrequest-restasyncresponse-nodes>
+
+
+
+  - **:** <>
+  - **:** <>
+  - **:** <>
